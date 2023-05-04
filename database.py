2e26d195ca6658a648fec303a3e80d15dc896def
Chandan Saini <47878598+getsuga-tenshou@users.noreply.github.com>
2023-05-04 18:26:54 +0200

    Update database.py

--- database.py
+++ database.py
@@ -115,13 +115,13 @@ class Database:
         else:
             return False
 
-    def sendRequest(self, from_user_id, to_user_id):
+        def sendRequest(self, from_user_id, to_user_id):
         '''
             Method for sending Request to a User
         '''
         request_data = (from_user_id, to_user_id, 0)
         add_request = """
-            INSERT INTO requests(from_user_id, to_user_id, status)
+            INSERT INTO request(from_user_id, to_user_id, status)
             VALUES(?, ?, ?);
         """
         self.curr.execute(add_request, request_data)
@@ -132,11 +132,11 @@ class Database:
             Method for Retrieving Sent Requests for a User
         '''
         sent_requests = """
-            SELECT requests.id, cred.username, cred.interests, cred.height, cred.preferences, cred.bio, requests.status 
-            FROM requests 
+            SELECT request.id, cred.username, cred.interests, cred.height, cred.preferences, cred.bio, request.status 
+            FROM request 
             INNER JOIN cred 
-            ON requests.to_user_id = cred.id 
-            WHERE requests.from_user_id = (?);
+            ON request.to_user_id = cred.id 
+            WHERE request.from_user_id = (?);
         """
         self.curr.execute(sent_requests, (from_user_id,))
         rows = self.curr.fetchall()
@@ -153,11 +153,11 @@ class Database:
             Method for Retrieving Received Requests for a User
         '''
         received_requests = """
-            SELECT requests.id, cred.username, cred.interests, cred.height, cred.preferences, cred.bio, requests.status 
-            FROM requests 
+            SELECT request.id, cred.username, cred.interests, cred.height, cred.preferences, cred.bio, request.status, request.to_user_id
+            FROM request 
             INNER JOIN cred 
-            ON requests.from_user_id = cred.id 
-            WHERE requests.to_user_id = (?);
+            ON request.from_user_id = cred.id 
+            WHERE request.to_user_id = (?);
         """
         self.curr.execute(received_requests, (to_user_id,))
         rows = self.curr.fetchall()
@@ -168,13 +168,14 @@ class Database:
                 print(f"{row[0]}\t{row[1]}\t{row[2]}\t{row[3]}cm\t{row[4]}\t{row[5][:25]}\t\t{row[6]}")
         else:
             print("You don't have any received requests yet.")
+        return rows
 
     def acceptRequest(self, request_id):
         '''
             Method for Accepting Friend Request
         '''
         accept_request = """
-            UPDATE requests 
+            UPDATE request 
             SET status = 1 
             WHERE id = (?);
         """
@@ -186,13 +187,13 @@ class Database:
             Method for Rejecting Friend Request
         '''
         reject_request = """
-            DELETE FROM requests 
+            DELETE FROM request 
             WHERE id = (?);
         """
         self.curr.execute(reject_request, (request_id,))
         self.conn.commit()
 
-    def createTable(self):
+        def createTables(self):
 
         '''
             Method for Creating Tables in Database
@@ -213,21 +214,36 @@ class Database:
             );
         '''
 
-        create_table_requests = '''
-            CREATE TABLE IF NOT EXISTS requests(
+        create_table_request = '''
+            CREATE TABLE IF NOT EXISTS request(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 from_user_id INTEGER NOT NULL,
                 to_user_id INTEGER NOT NULL,
-                status INTEGER NOT NULL,);
+                status INTEGER NOT NULL,
+                FOREIGN KEY (from_user_id) REFERENCES cred(id),
+                FOREIGN KEY (to_user_id) REFERENCES cred(id)
+            );
         '''
 
-        self.curr.execute(create_table_requests)
+        create_table_matches = '''
+            CREATE TABLE IF NOT EXISTS matches(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user1_id INTEGER,
+                user2_id INTEGER,
+                FOREIGN KEY (user1_id) REFERENCES cred(id),
+                FOREIGN KEY (user2_id) REFERENCES cred(id)
+            );
+        '''
+
+        self.curr.execute(create_table_cred)
+        self.curr.execute(create_table_request)
+        self.curr.execute(create_table_matches)
         self.conn.commit()
 
     def insertData(self, data):
 
         '''
-            Method for Insertig Data in Table in Database
+            Method for Inserting Data in Table in Database
         '''
 
         insert_data = """
@@ -292,37 +308,106 @@ class Database:
             Method for Retrieving Sent Requests for a User
         '''
         sent_requests = '''
-            SELECT c.id, c.username, r.status FROM requests r
+            SELECT c.id, c.username, r.status FROM request r
             JOIN cred c ON r.to_user_id = c.id
             WHERE r.from_user_id = (?);
         '''
         self.curr.execute(sent_requests, (user_id,))
         return self.curr.fetchall()
 
-    def getReceivedRequests(self, user_id):
+        def getReceivedRequests(self, user_id):
         '''
             Method for Retrieving Received Requests for a User
         '''
         received_requests = '''
-            SELECT c.id, c.username, r.status, r.id as request_id FROM requests r
-            JOIN cred c ON r.from_user_id = c.id
-            WHERE r.to_user_id = (?);
-        '''
-        self.curr.execute(received_requests, (user_id,))
-        return self
-    
-    def getReceivedRequests(self, user_id):
-        '''
-            Method for Retrieving Received Requests for a User
-        '''
-        received_requests = '''
-            SELECT c.id, c.username, r.status, r.id as request_id FROM requests r
+            SELECT c.id, c.username, r.status, r.id as request_id FROM request r
             JOIN cred c ON r.from_user_id = c.id
             WHERE r.to_user_id = (?);
         '''
         self.curr.execute(received_requests, (user_id,))
         return self.curr.fetchall()
-    
-   
 
+    def sendRequest(self, from_user_id, to_user_id):
+        '''
+            Method for sending Request to a User
+        '''
+        request_data = (from_user_id, to_user_id, 0)
+        add_request = """
+            INSERT INTO request(from_user_id, to_user_id, status)
+            VALUES(?, ?, ?);
+        """
+        self.curr.execute(add_request, request_data)
+        self.conn.commit()
+
+    def acceptRequest(self, request_id):
+        '''
+            Method for Accepting Friend Request
+        '''
+        accept_request = """
+            UPDATE request 
+            SET status = 1 
+            WHERE id = (?);
+        """
+        self.curr.execute(accept_request, (request_id,))
+        self.conn.commit()
+
+        request = self.getRequest(request_id)
+
+        # check if the request is accepted by the other user as well
+        if request[3] == 1:
+            user1_id = request[1]
+            user2_id = request[2]
+            self.createMatch(user1_id, user2_id)
+
+    def getRequest(self, request_id):
+        '''
+            Method for Retrieving a Request
+        '''
+        get_request = '''
+            SELECT * FROM request WHERE id = (?);
+        '''
+        self.curr.execute(get_request, (request_id,))
+        return self.curr.fetchone()
+
+    def rejectRequest(self, request_id):
+        '''
+            Method for Rejecting Friend Request
+        '''
+        reject_request = """
+            DELETE FROM request 
+            WHERE id = (?);
+        """
+        self.curr.execute(reject_request, (request_id,))
+        self.conn.commit()
+
+    def createMatch(self, user1_id, user2_id):
+        '''
+            Method for Creating Match between two users
+        '''
+        insert_match = """
+            INSERT INTO matches(user1_id, user2_id)
+            VALUES(?, ?);
+        """
+        self.curr.execute(insert_match, (user1_id, user2_id))
+        self.conn.commit()
+
+    def getMatches(self, user_id):
+        '''
+            Method for Retrieving Matches for a User
+        '''
+        matches_query = """
+            SELECT c.id, c.username, c.age, c.interests, c.height, c.smoking, c.drinking, c.preferences, c.bio
+            FROM cred c
+            WHERE c.id IN (
+                SELECT m.user2_id 
+                FROM matches m 
+                WHERE m.user1_id = (?)
+                UNION
+                SELECT m.user1_id 
+                FROM matches m 
+                WHERE m.user2_id = (?)
+            );
+        """
+        self.curr.execute(matches_query, (user_id, user_id))
+        return self.curr.fetchall()
 
